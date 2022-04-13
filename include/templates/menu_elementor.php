4fd21306e48feb5f2558c70e69a129c7416ea6c4
AndreyTSpb <tynyanyi@mail.ru>
2022-04-13 23:35:49 +0300

    стили и меню

--- include/templates/menu_elementor.php
+++ include/templates/menu_elementor.php
@@ -9,11 +9,11 @@
 <nav id="user-menu" migration_allowed="1" migrated="0" role="navigation" class="elementor-nav-menu--main elementor-nav-menu__container elementor-nav-menu--layout-horizontal e--pointer-underline e--animation-fade">
     <ul class="elementor-nav-menu" data-smartmenus-id="16498522835521367">
         <?php foreach ($menu_item_uuu as $item_uuu):?>
-            <li class="math menu-item menu-item-type-post_type menu-item-object-page menu-item-2120">
+            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2120">
                 <a id="<?=$item_uuu['id'];?>" href="<?=$item_uuu['link']?>" class="elementor-item" target="_blank"><?=$item_uuu['name']?></a>
             </li>
         <?php endforeach;?>
-        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4173">
+        <li class="menu-item-fio menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4173">
             <a href="#" class="elementor-item has-submenu" id="sm-16498522835521367-1" aria-haspopup="true" aria-controls="sm-16498522835521367-2" aria-expanded="false">
                 <i aria-hidden="true" class="fas fa-user"></i> <span id="user_name_header_menu">Пупкин Васько !</span><span class="sub-arrow"><i class="fas fa-caret-down"></i></span>
             </a>
